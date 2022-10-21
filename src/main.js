61812c6d599c0ae45055fd7badbfbd1f5cb8cafb
luanayasmim <luanayasmim11@outlook.com>
2022-10-21 16:34:06 -0300

    added more cards

--- src/main.js
+++ src/main.js
@@ -55,14 +55,49 @@ const cardNumber = document.querySelector('#card-number');
 const cardNumberPattern={
     mask: [
         {
-            mask: "0000 0000 0000 0000",
-            regex: /^4\d{0,15}/ ,
-            cardType: "visa"
+            mask: '0000 000000 00000',
+            regex: /^3[47]\d{0,13}/,
+            cardtype: 'american express'
         },
         {
-            mask: "0000 0000 0000 0000",
+            mask: '0000 0000 0000 0000',
+            regex: /^(?:6011|65\d{0,2}|64[4-9]\d?)\d{0,12}/,
+            cardtype: 'discover'
+        },
+        {
+            mask: '0000 000000 0000',
+            regex: /^3(?:0([0-5]|9)|[689]\d?)\d{0,11}/,
+            cardtype: 'diners'
+        },
+        {
+            mask: '0000 0000 0000 0000',
             regex: /^(5[1-5]\d{0,2}|22[2-9]\d{0,1}|2[3-7]\d{0,2})\d{0,12}/,
-            cardType: "mastercard"
+            cardtype: 'mastercard'
+        },
+        {
+            mask: '0000 000000 00000',
+            regex: /^(?:2131|1800)\d{0,11}/,
+            cardtype: 'jcb15'
+        },
+        {
+            mask: '0000 0000 0000 0000',
+            regex: /^(?:35\d{0,2})\d{0,12}/,
+            cardtype: 'jcb'
+        },
+        {
+            mask: '0000 0000 0000 0000',
+            regex: /^(?:5[0678]\d{0,2}|6304|67\d{0,2})\d{0,12}/,
+            cardtype: 'maestro'
+        },
+        {
+            mask: '0000 0000 0000 0000',
+            regex: /^4\d{0,15}/,
+            cardtype: 'visa'
+        },
+        {
+            mask: '0000 0000 0000 0000',
+            regex: /^62\d{0,14}/,
+            cardtype: 'unionpay'
         },
         {
             mask: "0000 0000 0000 0000",
